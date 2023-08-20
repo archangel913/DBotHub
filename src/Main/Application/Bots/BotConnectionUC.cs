348cb8d9c9c8089211b2edb2f91ad5e9332c80fc
archangel913 <archangel.yt.0913@gmail.com>
2023-08-20 14:22:15 +0900

    :arrow_double_down: ボットを接続するユースケースの追加

---
+++ src/Main/Application/Bots/BotConnectionUC.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using DBotHub.Domain.Bots;
+using DBotHub.Application.Bots;
+
+namespace Application.Bots;
+public class BotConnectionUC
+{
+    private IBotConnector BotConnector { get; }
+
+    private IBotRepository BotRepository { get; }
+
+    private Bot? Bot { get; }
+
+    public BotConnectionUC(IServiceProvider services)
+    {
+        this.BotRepository = services.GetRequiredService<IBotRepository>();
+        this.BotConnector = services.GetRequiredService<IBotConnector>();
+    }
+
+    public async Task<bool> ConnectAsync(string id, string name)
+    {
+        
+        // this.Bot = 　ファクトリーに任せる
+        return await this.Bot.ConnectAsync();
+    }
+
+    public async Task<bool> DisconnectAsync()
+    {
+        if(this.Bot is not null)
+        {
+            return await this.Bot.DisconnectAsync();
+        }
+        return false;
+    }
+}
---
+++ src/Main/Application/LaunchConfigurations/LaunchConfigurationUC.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.LaunchConfigurations;
+public class LaunchConfigurationUC
+{
+}
